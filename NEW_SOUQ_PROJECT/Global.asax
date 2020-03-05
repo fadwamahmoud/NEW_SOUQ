dd7609829884ad73fe4e51dabb78d0238bd1ffb1
Fadwa <fadwa.weasley@gmail.com>
2020-03-05 11:47:37 +0200

    Add project files.

---
+++ NEW_SOUQ_PROJECT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NEW_SOUQ_PROJECT.MvcApplication" Language="C#" %>
---
+++ NEW_SOUQ_PROJECT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NEW_SOUQ_PROJECT.Startup))]
+namespace NEW_SOUQ_PROJECT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
